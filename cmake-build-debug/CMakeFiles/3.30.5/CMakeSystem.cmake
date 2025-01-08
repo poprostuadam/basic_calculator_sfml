8124e6ef80c486372a63d966d63764f2a773d6fa
po.prostu.adam <adam.stefcio@gmail.com>
2025-01-08 10:12:01 +0100

    CMake changes and basic classes

--- cmake-build-debug/CMakeFiles/3.30.5/CMakeSystem.cmake
+++ cmake-build-debug/CMakeFiles/3.30.5/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.12.4-1-MANJARO")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-
+include("/home/adam/.vcpkg-clion/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
 set(CMAKE_SYSTEM "Linux-6.12.4-1-MANJARO")
 set(CMAKE_SYSTEM_NAME "Linux")
